628735413f37101cbe1e1539237256e928505bd9
AayushiShukla7 <AayushiShukla27@gmail.com>
2022-10-09 19:30:37 -0300

    Savepoint #1

---
+++ RazorPizzeria/Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Forms
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
